72ddb37f17d0dfc5d15566b5fd0c574ee58e659e
shreyanshu001 <shuklashreyanshu04@gmail.com>
2025-12-24 11:39:30 +0530

    weather state

---
+++ hudle_assign/lib/blocs/weather_state.dart
@@ -0,0 +1,35 @@
+import 'package:equatable/equatable.dart';
+import '../models/weather_model.dart';
+
+abstract class WeatherState extends Equatable {
+  const WeatherState();
+
+  @override
+  List<Object?> get props => [];
+}
+
+class WeatherInitial extends WeatherState {
+  const WeatherInitial();
+}
+
+class WeatherLoading extends WeatherState {
+  const WeatherLoading();
+}
+
+class WeatherLoaded extends WeatherState {
+  final WeatherModel weather;
+
+  const WeatherLoaded(this.weather);
+
+  @override
+  List<Object?> get props => [weather];
+}
+
+class WeatherError extends WeatherState {
+  final String message;
+
+  const WeatherError(this.message);
+
+  @override
+  List<Object?> get props => [message];
+}
